dbf7eb86374445cb9f61dd2beb638fc9ce216d69
Radite <joshuameghoo@gmail.com>
2024-06-12 20:16:13 -0500

    Style change

--- frontend/anime-recommendation-website/src/components/RecommendationPage/RecommendationPage.js
+++ frontend/anime-recommendation-website/src/components/RecommendationPage/RecommendationPage.js
@@ -2,19 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Header from '../Header';
 import SearchBar from './SearchBar';
 import AnimeCard from './AnimeCard';
-import AltAnimeCard from './AltAnimeCard'; // Import the AltAnimeCard component
-import Pagination from './Pagination';
 import '../../styles/RecommendationPage.css';
 import fetchAnimeNames from '../../utils/fetchAnimeNames';
-import { handleSearch, handleRecommendation, handleSuggestionClick, handleBlur, paginate } from '../../utils/animeFunctions';
+import { handleSearch, handleRecommendation, handleSuggestionClick, handleBlur } from '../../utils/animeFunctions';
 
 function RecommendationPage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [animeList, setAnimeList] = useState([]);
   const [suggestedAnime, setSuggestedAnime] = useState([]);
   const [recommendedAnime, setRecommendedAnime] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [recommendationsPerPage] = useState(6);
   const [loading, setLoading] = useState(false);
   const [generatedRecommendations, setGeneratedRecommendations] = useState(false); // State to track if recommendations were generated
   const [suggestionClicked, setSuggestionClicked] = useState(false); // State to track if a suggestion was clicked
@@ -32,10 +28,35 @@ function RecommendationPage() {
     fetchData();
   }, []);
 
+  // Function to handle recommendation fetching
+  const handleRecommendations = async () => {
+    try {
+      setLoading(true);
+      const recommendations = await handleRecommendation(searchTerm);
+      setRecommendedAnime(recommendations);
+      setLoading(false);
+      setGeneratedRecommendations(true);
+    } catch (error) {
+      console.error('Error fetching recommendations:', error);
+      setLoading(false);
+    }
+  };
+
+  // useEffect to fetch recommendations when searchTerm changes
+  useEffect(() => {
+    if (searchTerm !== '') {
+      handleRecommendations();
+    } else {
+      setRecommendedAnime([]);
+      setGeneratedRecommendations(false);
+    }
+  }, [searchTerm]);
+
   return (
     <div>
       <Header />
-      <div className="recommendation-container">
+      <div className="search-bar-container">
+        <div className="overlay"></div>
         <SearchBar
           searchTerm={searchTerm}
           handleSearch={(e) => {
@@ -51,30 +72,25 @@ function RecommendationPage() {
           handleSuggestionClick={(animeName) => {
             handleSuggestionClick(animeName, animeList, setSearchTerm, setSuggestedAnime, handleRecommendation, setLoading, setRecommendedAnime);
             setSuggestionClicked(true); // Set suggestionClicked state to true when suggestion is clicked
-            setCurrentPage(1); // Reset currentPage to 1 when a new suggestion is clicked
           }}
-          
         />
-
-<div className="anime-list">
-  {loading ? (
-    <div className="loading-placeholder">Loading...</div>
-  ) : (
-    <div className="anime-cards">
-      {recommendedAnime.slice((currentPage - 1) * recommendationsPerPage, currentPage * recommendationsPerPage).map((anime, index) => (
-        <AnimeCard key={index} anime={anime} />
-      ))}
-    </div>
-  )}
-</div>
-<Pagination
-  currentPage={currentPage}
-  recommendedAnime={recommendedAnime}
-  recommendationsPerPage={recommendationsPerPage}
-  paginate={setCurrentPage}
-/>
-
       </div>
+
+      {suggestionClicked && ( // Conditionally render recommendation container based on suggestionClicked state
+        <div className="recommendation-container">
+          <div className="anime-list">
+            {loading ? (
+              <div className="loading-placeholder">Loading...</div>
+            ) : (
+              <div className="anime-cards">
+                {recommendedAnime.map((anime, index) => (
+                  <AnimeCard key={index} anime={anime} />
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
